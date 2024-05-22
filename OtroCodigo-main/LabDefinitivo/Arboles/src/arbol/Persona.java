8a2a4861b3154c9f907e25d9318ccd1e1960de15
LEGCIUS <143227122+LEGCIUS@users.noreply.github.com>
2024-05-22 12:57:09 -0600

    Add files via upload

---
+++ OtroCodigo-main/LabDefinitivo/Arboles/src/arbol/Persona.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arbol;
+
+/**
+ *
+ * @author pablonoguera
+ * editado por Larry the DOG
+ */
+public class Persona {
+    private int cedula;
+
+    public Persona(int  cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+}
